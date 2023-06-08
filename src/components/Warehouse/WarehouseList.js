608a4efa9f25af1128b54b97cad8b279337fd857
mikemazara <mike.mazara@gmail.com>
2023-06-07 22:22:01 -0700

    mobile styling

--- src/components/Warehouse/WarehouseList.js
+++ src/components/Warehouse/WarehouseList.js
@@ -61,65 +61,68 @@ const WarehouseList = () => {
           </Link>
         </div>
         <div className="warehouse__list-container">
-          <div className="warehouse__table">
-            {/* {warehouses.map((warehouse) => <WarehouseListItem warehouse={warehouse}/> )} */}
-            {warehouses.map((warehouse) => {
-              const {
-                id,
-                warehouse_name,
-                address,
-                city,
-                country,
-                contact_name,
-                contact_phone,
-                contact_email,
-              } = warehouse;
-              return (
-                <article key={warehouse.id} className="warehouse__article">
-                  <div className="warehouse__name-address">
-                    <h3 className="warehouse__name-header">Warehouse</h3>
-                    <Link to={`/warehouse/${id}`}>
-                      <h2>{warehouse_name}</h2>
-                    </Link>
-                    <Link to={`/warehouse/${id}`}>
+          {/* {warehouses.map((warehouse) => <WarehouseListItem warehouse={warehouse}/> )} */}
+          {warehouses.map((warehouse) => {
+            const {
+              id,
+              warehouse_name,
+              address,
+              city,
+              country,
+              contact_name,
+              contact_phone,
+              contact_email,
+            } = warehouse;
+            return (
+              <article key={warehouse.id} className="warehouse__article">
+                <div className="warehouse__name-address">
+                  <h3 className="warehouse__name-header">WAREHOUSE</h3>
+                  <Link
+                    to={`/warehouse/${id}`}
+                    className="warehouse__name-link"
+                  >
+                    <h2 className="warehouse__name">
+                      {warehouse_name}{" "}
                       <img
                         className="warehouse__arrow-icon"
                         src={arrowIcon}
                         alt="arrow icon"
                       />
-                    </Link>
-                    <h3>Address</h3>
-                    <p>
-                      {address}, {city}, {country}
-                    </p>
-                  </div>
-                  <div className="warehouse__contact">
-                    <h3>CONTACT NAME</h3>
-                    <p>{contact_name}</p>
-                    <h3>CONTACT INFORMATION</h3>
-                    <p>{contact_phone}</p>
-                    <p>{contact_email}</p>
-                  </div>
-                  <div className="warehouse__actions">
-                    <Link to={`/warehouse/delete/${warehouse.id}`}>
-                      <img
-                        className="warehouse__delete-icon"
-                        src={deleteIcon}
-                        alt="delete icon"
-                      />
-                    </Link>
-                    <Link to={`/warehouse/edit/${warehouse.id}`}>
-                      <img
-                        className="warehouse__edit-icon"
-                        src={editIcon}
-                        alt="edit icon"
-                      />
-                    </Link>
-                  </div>
-                </article>
-              );
-            })}
-          </div>
+                    </h2>
+                  </Link>
+                  <h3 className="warehouse__name-header">ADDRESS</h3>
+                  <p className="warehouse__info">
+                    {address}, {city}, {country}
+                  </p>
+                </div>
+                <div className="warehouse__contact">
+                  <h3 className="warehouse__name-header">CONTACT NAME</h3>
+                  <p className="warehouse__info">{contact_name}</p>
+                  <h3 className="warehouse__name-header">
+                    CONTACT INFORMATION
+                  </h3>
+                  <p className="warehouse__info">{contact_phone}</p>
+                  <p className="warehouse__info">{contact_email}</p>
+                </div>
+                <div className="warehouse__actions">
+                  <Link to={`/warehouse/delete/${warehouse.id}`}>
+                    <img
+                      className="warehouse__delete-icon"
+                      src={deleteIcon}
+                      alt="delete icon"
+                    />
+                  </Link>
+                  <Link to={`/warehouse/edit/${warehouse.id}`}>
+                    <img
+                      className="warehouse__edit-icon"
+                      src={editIcon}
+                      alt="edit icon"
+                    />
+                  </Link>
+                </div>
+              </article>
+            );
+          })}
         </div>
       </div>
     </div>
