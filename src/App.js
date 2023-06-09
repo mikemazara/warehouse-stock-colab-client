ad4e9984951dcc88a05d086dbb772ffc462a2e41
Sweta <spmac@swetas-mbp.lan>
2023-06-08 21:16:36 -0400

    complete inventory detail component styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WarehousesPage from "./pages/WarehousesPage/WarehousesPage";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import InventoryDetailsPage from "./pages/InventoryDetailsPage/InventoryDetailsPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import './App.scss';
@@ -14,7 +15,7 @@ function App() {
           <Route path="/" element={<WarehousesPage />}/>
           <Route path="/warehouses/:id" element={<WarehousesPage />} />
           <Route path="/inventory" element={<InventoryPage />}/>
-          <Route path="/inventory/:id" element={<InventoryPage />}/>
+          <Route path="/inventory/:id" element={<InventoryDetailsPage />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
