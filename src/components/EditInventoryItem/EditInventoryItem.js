cd5b7751d8a6489da289350f376c2e861ff41f19
Sweta <spmac@swetas-mbp.lan>
2023-06-12 17:22:59 -0400

    change cursors, add hover effects to buttons, and fix link paths

--- src/components/EditInventoryItem/EditInventoryItem.js
+++ src/components/EditInventoryItem/EditInventoryItem.js
@@ -135,7 +135,7 @@ const EditInventoryItem = () => {
   return (
     <div className="edit-item">
       <div className="edit-item__header">
-        <NavLink to={`/inventories/${id}`} className="edit-item__link">
+        <NavLink to={`/inventory/${id}`} className="edit-item__link">
           <button className="edit-item__arrow-button">
             <img
               className="edit-item__arrow"
@@ -290,7 +290,7 @@ const EditInventoryItem = () => {
         </div>
         <div className="edit-item__buttons-container">
           <NavLink
-            to={`/inventories/${id}`}
+            to={`/inventory/${id}`}
             className="edit-item__button edit-item__button--cancel edit-item__link"
           >
             Cancel
