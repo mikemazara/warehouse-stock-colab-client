3c223f5f91aa12a6a4fcb43d1b1c7ecefed2a0a4
mikemazara <mike.mazara@gmail.com>
2023-06-12 12:28:45 -0700

    touch up warehouse list

--- src/components/Warehouse/WarehouseList.js
+++ src/components/Warehouse/WarehouseList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import useWarehouses from "../API functions/WarehouseApi";
+import useWarehouses from "../ApiFunctions/WarehouseApi";
 import editIcon from "../../assets/icons/edit-blue24px.svg";
 import arrowIcon from "../../assets/icons/chevron_right-24px.svg";
 
